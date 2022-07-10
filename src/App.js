4ce620a356a7e9825f82a6e314cab1faa9753ecb
Aaron Johnson <sugarwaterpurple.js@gmail.com>
2022-07-10 12:50:03 -0700

    add box component and import into app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Box from './components/Box';
+
 function App() {
 	return (
-		<div>
-			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
+		<div className='flex justify-start gap-5'>
+			
 		</div>
 	);
 }
---
+++ src/components/Box.jsx
@@ -0,0 +1,7 @@
+// import { useState } from 'react';
+
+const Box = ({ color }) => {
+	return <div className={`h-48 w-48 bg-${color}-500`}>&nbsp;</div>;
+};
+
+export default Box;
